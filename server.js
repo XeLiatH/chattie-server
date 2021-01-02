e877abc88cbd5d30423b1136572118e5cc760c21
Matěj Beran <matej.beran96@gmail.com>
2021-01-02 15:00:03 +0100

    Install mysql and prepare routes.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const mysql = require('mysql');
 
 const config = require('./config.json');
 
@@ -12,8 +13,66 @@ const io = socketio(server, {
     cors: config.cors
 });
 
+// Establish database connection
+const connection = mysql.createConnection(config.mysql);
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+    if (err) throw err
+
+    console.log('The solution is: ', rows[0].solution)
+});
+
+connection.end();
+
 // Set static folder to public
 app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.get('/users', (req, res) => {
+    res.json([
+        {
+            id: 1,
+            email: "matej.beran@tul.cz"
+        }
+    ]);
+});
+
+app.get('/chat/direct', (req, res) => {
+    
+});
+
+app.post('/chat/direct', (req, res) => {
+    
+});
+
+app.delete('/chat/direct/:directChatId', (req, res) => {
+    // req.params.directChatId
+});
+
+app.post('/chat/direct/:directChatId/messeges', (req, res) => {
+    
+});
+
+app.get('/chat/groups', (req, res) => {
+    
+});
+
+app.post('/chat/groups', (req, res) => {
+    
+});
+
+app.put('/chat/groups/:chatGroupId', (req, res) => {
+    
+});
+
+app.delete('/chat/groups/:chatGroupId', (req, res) => {
+    
+});
+
+app.get('/chat/groups/:chatGroupId/messeges', (req, res) => {
+    
+});
 
 // some actual code ??
 io.on('connection', socket => {
