bb0d376f162232a7929351c7285c1aeaf87fe9bc
Matěj Beran <matej.beran96@gmail.com>
2021-01-02 12:10:04 +0100

    Start with a config add CORS headers to support clients from different origins.

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 
+const config = require('./config.json');
+
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+// todo: CORS should probably not be included in final build?
+const io = socketio(server, {
+    cors: config.cors
+});
 
 // Set static folder to public
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,6 +18,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 // some actual code ??
 io.on('connection', socket => {
     console.log('Client connected to the f-ing server.');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected from the f-ing server.');
+    });
 });
 
 const PORT = 8888 || process.env.PORT;
