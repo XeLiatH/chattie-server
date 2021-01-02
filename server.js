5dad42198f63cfcaf57bff24dcc64d31efbcaf8d
Matěj Beran <matej.beran96@gmail.com>
2021-01-02 11:44:48 +0100

    Prepare public folder and socket.io integration.

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder to public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// some actual code ??
+io.on('connection', socket => {
+    console.log('Client connected to the f-ing server.');
+});
 
 const PORT = 8888 || process.env.PORT;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
